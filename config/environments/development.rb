36923aa3a00151754b5fc68ec9e69e438bf6bced
Rafael <rejimenez.12@gmail.com>
2016-04-21 04:02:54 -0430

    agregando configuracion de correo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,23 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.perform_deliveries = true # Set it to false to disable the email in dev mode
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default charset: "utf-8"
+
+  #en producción colocar la dirección del dominio
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.assets.debug = true
+ 
+   config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["DOMAIN_NAME"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+
 end
